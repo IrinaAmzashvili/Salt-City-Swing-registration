5ec75a6e400098732e56a0dda37cb8e6639a0d62
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-17 14:43:05 -0600

    Complete signup validation and phase 5

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post('/', asyncHandler(async (req, res) => {
+const validateSignup = [
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide your first name.')
+        .isLength({ min: 2, max: 30 })
+        .withMessage('First name must be a minimum of 2 characters and a maximum of 30 characters long.'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide your last name.')
+        .isLength({ min: 2, max: 30 })
+        .withMessage('Last name must be a minimum of 2 characters and a maximum of 30 characters long.'),
+    // must check for unique username
+    check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an email address.')
+        .isEmail()
+        .withMessage('Please provide a valid email address.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+        .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'),
+    check('vaxCardImg')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an image of your vaccination card.'),
+    handleValidationErrors,
+];
+
+router.post('/', validateSignup, asyncHandler(async (req, res) => {
     const { firstName, lastName, email, password, vaxCardImg, userPhoto } = req.body;
     const user = await User.signup({ firstName, lastName, email, password, vaxCardImg, userPhoto });
 
