f2f7dcc7f6fa10d3610f14265c9d01ba96c8cf35
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-17 14:08:58 -0600

    Complete user signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,17 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+router.post('/', asyncHandler(async (req, res) => {
+    const { firstName, lastName, email, password, vaxCardImg, userPhoto } = req.body;
+    const user = await User.signup({ firstName, lastName, email, password, vaxCardImg, userPhoto });
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+}))
 
 module.exports = router;
