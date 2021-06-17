b756714b8db387aadf98bd3153e6d9243a7670b3
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-17 13:58:41 -0600

    Complete login api route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,12 +2,12 @@ const asyncHandler = require('express-async-handler');
 const router = require("express").Router();
 const { User } = require('../../db/models');
 const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
-// router.post("/test", (req, res) => {
-//     res.json({ requestBody: res.body });
-// });
-
+router.use('/users', usersRouter);
 
 
 module.exports = router;
