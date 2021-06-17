d5dbd6b94f94224ab11d2a183980db34a9dfb1e6
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-17 15:35:14 -0600

    Complete frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
 import App from './App';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
