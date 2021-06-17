842fcc708baca64549ff09ab90653558f96055fa
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-17 14:12:45 -0600

    Complete get session user and Phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,4 +30,14 @@ router.delete('/', (_req, res) => {
     return res.json({ message: 'success' });
 })
 
+// get session
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    } else return res.json({});
+})
+
 module.exports = router;
