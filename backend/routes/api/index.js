69d17e785c91243a80169b76d1d137f346efe714
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-17 11:31:51 -0600

    Complete phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,13 @@
+const asyncHandler = require('express-async-handler');
 const router = require("express").Router();
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
 
-router.post("/test", (req, res) => {
-    res.json({ requestBody: res.body });
-});
+
+// router.post("/test", (req, res) => {
+//     res.json({ requestBody: res.body });
+// });
+
+
 
 module.exports = router;
