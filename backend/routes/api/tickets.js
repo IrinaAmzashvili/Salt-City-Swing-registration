5642f89df93dfcb6c52d06d5a018a7c118ab3136
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-06-24 14:11:38 -0600

    Implement get all tickets api route

--- backend/routes/api/tickets.js
+++ backend/routes/api/tickets.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { Ticket } = require('../../db/models');
+const { Ticket, Class } = require('../../db/models');
 
 const router = express.Router();
+
+router.get('/:userId', asyncHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const tickets = await Ticket.findAll({
+        where: { userId },
+        include: Class,
+    });
+    res.json(tickets);
+}));
 
 router.post('/', asyncHandler(async (req, res) => {
     const ticket = req.body;
